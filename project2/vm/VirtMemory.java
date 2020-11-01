c8c54dcbff79b0edb886b5185f3aa18b10638df6
muirrin <muiroconnell@gmail.com>
2020-11-01 14:30:47 -0500

    first commit

---
+++ project2/vm/VirtMemory.java
@@ -0,0 +1,12 @@
+public class VirtMemory extends Memory {
+
+public VirtMemory(){
+	
+}
+
+void write(int addr, byte value){
+	//puts value into addr
+	//if not in mem, throw page fault and load page from disk to phs mem
+}
+
+}
