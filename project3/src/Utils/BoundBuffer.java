6ddb0c630d50db7123f226ed650ca00829fb6e79
Stella <ss56127n@pace.edu>
2020-12-05 15:04:31 -0500

    added comments to bounded buffer

--- project3/src/Utils/BoundBuffer.java
+++ project3/src/Utils/BoundBuffer.java
@@ -9,24 +9,25 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BoundBuffer <T> {
+     //Please note the biggest difference between this BoundBuffer
+     //and the one we demoed in class is <T>
+     // implement member functions: deposit() and fetch()
+     
+     //Synchronization Attributes
      private Lock mutex_lock = new ReentrantLock(true);
      private final Condition isEmpty;
      private final Condition isFull;
 
-   //Please note the biggest difference between this BoundBuffer
-   //and the one we demoed in class is <T>
-   // implement member functions: deposit() and fetch()
-
-   int currentSize; 
-   T[] circularQueueItems;
-   int size; 
-
-   private int rear;//rear position of circular queue
-   private int front; //front position of circular queue     
+     //Circular Queue Attributes
+     int currentSize; 
+     T[] circularQueueItems;
+     int size; 
+     private int rear;//rear position of circular queue
+     private int front; //front position of circular queue     
 
 
-   //Constructor for the Bound Buffer
-   public BoundBuffer(int size){ 
+     //Constructor for the Bound Buffer
+     public BoundBuffer(int size){ 
         this.size = size;
         circularQueueItems = (T[]) new Object[this.size];
         currentSize = 0;
@@ -34,9 +35,10 @@ public class BoundBuffer <T> {
         rear = 0;
         isEmpty = mutex_lock.newCondition();
         isFull = mutex_lock.newCondition();
-   }
+     }
 
-   public void deposit(T item) throws InterruptedException {
+     //Deposit method 
+     public void deposit(T item) throws InterruptedException {
         mutex_lock.lock();
         while(currentSize == size){ //if full
            isFull.await();
@@ -46,10 +48,11 @@ public class BoundBuffer <T> {
          currentSize++; // increase number of elements in Circular queue
          isEmpty.signal();
          mutex_lock.unlock();
-        }
+     }
    
 
-   public T fetch() throws InterruptedException {
+     //Fetch method
+     public T fetch() throws InterruptedException {
         mutex_lock.lock();
         while(size == 0){ //if full
             isEmpty.await();
@@ -62,7 +65,7 @@ public class BoundBuffer <T> {
         isFull.signal();
         mutex_lock.unlock();
         return item;
-   }
+     }
 
 //    public boolean isFull() {
 //         return (currentSize == circularQueueItems.length);
