812763c4ca183703d771dbe03168a4d71e80972d
Stella <ss56127n@pace.edu>
2020-12-06 23:19:24 -0500

    Fixed

--- project3/src/MyMapReduce.java
+++ project3/src/MyMapReduce.java
@@ -2,25 +2,38 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.logging.Level;
-public class MyMapReduce extends MapReduce {
-	////TODO: your code here.
-	//	What is in a running instance of MapReduce?
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MyMapReduce extends MapReduce implements Runnable {
+	//// TODO: your code here.
+	// What is in a running instance of MapReduce?
 	static PartitionTable table;
 	static int numPartitions;
+	private Lock mutex_lock = new ReentrantLock(true);
 
-	public void MREmit(Object key, Object value)
-	{
-		//TODO: your code here.
-		//call partitionTable
+	public void MREmit(Object key, Object value) {
+		// TODO: your code here.
+		// call partitionTable addToPartition(Object key, value, partitionNumber);
+		mutex_lock.lock();
+		long partitionNumber = Partitioner(key, value, numPartitions); // Get the partition number
+		KV item = new KV(key, value);
+		table.addToPartition(item, partitionNumber); // Add to partition table which will be added to a buffer
+		mutex_lock.unlock();
 		throw new UnsupportedOperationException();
 	}
 
-	public Object MRGetNext(Object key, int partition_number) {
-		//TODO: your code here.
-		//return value
-		return null;
-		throw new UnsupportedOperationException();
-	}
+	public Object MRGetNext(Object key, int partition_number)  {
+		// TODO: your code here.
+		mutex_lock.lock();
+		KV item;
+		item = table.fetchFromPartition(key, partition_number);
+		mutex_lock.unlock();
+		return item;
+		//throw new UnsupportedOperationException(); ??? 
+  }
 	@Override
 	protected void MRRunHelper(String inputFileName,
 		    		  MapperReducerClientAPI mapperReducerObj,
@@ -28,22 +41,40 @@ public class MyMapReduce extends MapReduce {
 		    		  int num_reducers)
 	{
 		//TODO: your code here.
-
+    int i = 0;
+    while(i<num_mappers){
+      Thread producer = new Thread(); //mapper threads
+      producer.start();
+      i++;
+    }
+    i=0;
+    while(i<num_reducers){
+    	Thread consumer = new Thread();
+      consumer.start();
+      i++;
+    }
 		throw new UnsupportedOperationException();
-	}
-
+	} 
 	//This is the producer
 	public void Mapper(Object inputSource) {
 	//public class ConcurrentHashMap<K,​V> extends AbstractMap<K,​V> implements ConcurrentMap<K,​V>, Serializable
 	//ConcurrentHashMap<K, V> chm = new ConcurrentHashMap<>();
 	//int counter = num of mapper threads; decrement every time thread completes
 			Map(inputSource);
+      
 	}
-
+  
 	//This is the consumer
 	public void Reducer(Object key, int partition_number){
-			reduce(key, partition_number);
-
+  	ConcurrentHashMap<Object, Object> hashMap = new ConcurrentHashMap<>();
+	KV item;
+    while(key!=EOS){ //before end of string
+    	item = table.fetchFromPartition(key, partition_number);
+      hashMap.put(item.key,item.val);
+    }
+			Reduce(key, partition_number);
+      System.out.println("This is KVS:");
 	}
+
 
 }
--- project3/src/PartitionTable.java
+++ project3/src/PartitionTable.java
@@ -5,40 +5,45 @@ import java.util.concurrent.locks.ReentrantLock;
 import Utils.*;
 
 public class PartitionTable {
-	//TODO: your code here
-	//Notes:
-	// (1) each partition works like an bounded buffer between mappers and a reducer. (you can assume size = 10 or 50)
-	// (2) if reducer_i wants to fetch a KV pair it can only fetches from partition_i
-	// (3) reducer_i also has to maintain a kv store which maps a key to all the value associated to the key, for instace
-	// if reducer_i has fetched {"foo", 1} {"bar",1} {"foo",1} {"foo",1} and {"bar",1}
-	// from partition_i, then reducer_i should have maintained {"foo", {1,1,1}} and {"bar", {1,1}}. You can use a
+	// TODO: your code here
+	// Notes:
+	// (1) each partition works like an bounded buffer between mappers and a
+	// reducer. (you can assume size = 10 or 50)
+	// (2) if reducer_i wants to fetch a KV pair it can only fetches from
+	// partition_i
+	// (3) reducer_i also has to maintain a kv store which maps a key to all the
+	// value associated to the key, for instace
+	// if reducer_i has fetched {"foo", 1} {"bar",1} {"foo",1} {"foo",1} and
+	// {"bar",1}
+	// from partition_i, then reducer_i should have maintained {"foo", {1,1,1}} and
+	// {"bar", {1,1}}. You can use a
 	// hashmap or a tree to implement this KV store, but where
 	// should this KV stored be? inside the reducer? inside this partitionTable?
-  
-  private Lock mutex_lock = new ReentrantLock(true);
-  private static int INITIAL_SIZE = 10;
-  BoundBuffer<KV> []boundBuffer = new BoundBuffer[INITIAL_SIZE];//Declaring an array of Bound Buffers
 
-  public PartitionTable(){
-     //public KV(Object key, Object value);
-     for (int i = 0; i < INITIAL_SIZE; i++) { //Initializing the array of bound buffers
-            boundBuffer[i] = new BoundBuffer<KV>(INITIAL_SIZE);
-     }
-  }
-	
-  public void addToPartition(KV item, long partitionNumber) throws InterruptedException {
-     mutex_lock.lock();     
-     //long partitionNumber = Partitioner(item.key,INITIAL_SIZE);
-	 boundBuffer[(int) partitionNumber].deposit(item);
-      mutex_lock.unlock();
-  }
+	private Lock mutex_lock = new ReentrantLock(true);
+	private static int INITIAL_SIZE = 10;
+	BoundBuffer<KV>[] boundBuffer = new BoundBuffer[INITIAL_SIZE];// Declaring an array of Bound Buffers
 
-  //Fetch method
-  public KV fetchFromPartition(Object key, int partitionNumber) throws InterruptedException {
-	 mutex_lock.lock();
-	 KV item;
-     item = boundBuffer[partitionNumber].fetch();
-     mutex_lock.unlock();
-     return item;
+	public PartitionTable() {
+		// public KV(Object key, Object value);
+		for (int i = 0; i < INITIAL_SIZE; i++) { // Initializing the array of bound buffers
+			boundBuffer[i] = new BoundBuffer<KV>(INITIAL_SIZE);
+		}
+	}
+
+	public void addToPartition(KV item, long partitionNumber) {
+		mutex_lock.lock();
+		// long partitionNumber = Partitioner(item.key,INITIAL_SIZE);
+		boundBuffer[(int) partitionNumber].deposit(item);
+		mutex_lock.unlock();
+	}
+
+	// Fetch method
+	public KV fetchFromPartition(Object key, int partitionNumber) {
+		mutex_lock.lock();
+		KV item;
+		item = boundBuffer[partitionNumber].fetch();
+     	mutex_lock.unlock();
+     	return item;
   }
 }
